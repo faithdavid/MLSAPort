9e3ca9bb021a78743ec452cfe3e208d1741a7586
faithdavid <59588415+faithdavid@users.noreply.github.com>
2023-07-29 02:11:28 +0000

    First commit for portfolio website

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,7 +24,7 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a Computer Science and Economics student studying at Obafemi Awolowo University. I love seeing how the things in the world can be made easier and more exciting.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my Software Development Lifecycle experience I continually look for new and better ways to make tech accessible by all.";
 
 const About = () => {
   return (
